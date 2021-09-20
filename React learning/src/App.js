d122e4ffb69db5234d94a618b355776d34922229
Deepak Tewatia <36035556+DpkTewatia@users.noreply.github.com>
2021-09-20 20:19:13 +0530

    first commit

---
+++ React learning/src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from "./MyComponents/header";
+import Footer from "./MyComponents/footer";
+import Todos from "./MyComponents/todos";
+
+
+
+function App() {
+  // let myVariable = 345;
+  return (
+    <>
+     <Header title = "Doubt Session" searchBar={true} /> 
+     <hr/>
+     <Todos/><hr/> 
+     <Footer/>  <hr/> 
+     
+       
+     
+     
+    </>
+  );
+}
+
+export default App;
---
+++ React learning/src/MyComponents/todos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function header() {
+    return (
+        <div className="container" >
+            <h3>
+            Todos works!                
+            </h3>
+        </div>
+    )
+}
